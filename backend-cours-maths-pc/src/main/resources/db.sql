5976011f7d5ce83c42de856aa175b30baa52b6f1
mboup <mboup124@gmail.com>
2024-07-05 18:08:35 +0200

    add contact form & Toastr

--- backend-cours-maths-pc/src/main/resources/db.sql
+++ backend-cours-maths-pc/src/main/resources/db.sql
@@ -8,5 +8,16 @@ CREATE TABLE IF NOT EXISTS contact_message (
 
 accès SSH sur Hostinger
 ssh -p 65002 u516404965@194.164.74.136
+u516404965
+194.164.74.136
+java -jar backend-cours-maths-pc-0.0.1-SNAPSHOT.jar
+
+cd public_html
+
+java -jar backend-cours-maths-pc-0.0.1-SNAPSHOT.jar
+
+
+https://damemboup.com/
+
 
 ssh u516404965@194.164.74.136
