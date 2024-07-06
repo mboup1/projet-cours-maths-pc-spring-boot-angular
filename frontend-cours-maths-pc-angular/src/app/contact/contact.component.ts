97b3128ac4e9b3e6c9fbcf750716b7ece345bc87
mboup <mboup124@gmail.com>
2024-07-06 17:05:15 +0200

    EmailJS

--- frontend-cours-maths-pc-angular/src/app/contact/contact.component.ts
+++ frontend-cours-maths-pc-angular/src/app/contact/contact.component.ts
@@ -3,6 +3,7 @@ import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms'
 import { HttpClient } from '@angular/common/http';
 import { Contact } from '../models/contact.model';
 import { ToastrService } from 'ngx-toastr';
+import emailjs, { EmailJSResponseStatus } from '@emailjs/browser';
 
 
 @Component({
@@ -18,7 +19,8 @@ export class ContactComponent {
   contactForm = new FormGroup({
     name: new FormControl('', [Validators.required]),
     email: new FormControl('', [Validators.required, Validators.email]),
-    phone: new FormControl('', [Validators.required, Validators.pattern('^[0-9]*$')]),
+    // phone: new FormControl('', [Validators.required, Validators.pattern('^[0-9]*$')]),
+    phone: new FormControl(''),
     message: new FormControl('', [Validators.required])
   });
 
@@ -26,25 +28,49 @@ export class ContactComponent {
 
   onSubmit() {
     if (this.contactForm.valid) {
-      const contact: Contact = this.contactForm.value as Contact;
-      this.http.post('http://localhost:8080/api/contact', contact)
-        .subscribe({
-          next: (response) => {
-            console.log('Contact message saved', response);
-            this.toastr.success('Votre message a été envoyé avec succès.');
+      const templateParams = {
+        from_name: this.contactForm.get('name')?.value,
+        reply_to: this.contactForm.get('email')?.value,
+        phone: this.contactForm.get('phone')?.value,
+        message: this.contactForm.get('message')?.value,
+      };
 
-
-            this.errorMessage = '';
-            this.contactForm.reset();
-
-          },
-          error: (error) => {
-            console.error('Error saving contact message', error);
-            this.errorMessage = 'Une erreur est survenue lors de l\'envoi du message. Réessayez !';
-            // this.toastr.error('Une erreur est survenue lors de l\'envoi du message. Réessayez');
-
-          }
+      emailjs.send('service_n2t0sj7', 'template_29c199i', templateParams, 'Z2rmah8bXQzQjaIRq')
+        .then((result: EmailJSResponseStatus) => {
+          console.log(result.text);
+          this.toastr.success('Votre message a été envoyé avec succès.');
+          this.errorMessage = '';
+          this.contactForm.reset();
+        }, (error) => {
+          console.error(error.text);
+          this.errorMessage = 'Une erreur est survenue lors de l\'envoi du message. Réessayez !';
+          this.toastr.error('Une erreur est survenue lors de l\'envoi du message. Réessayez');
         });
     }
   }
+
+
+  // onSubmit() {
+  //   if (this.contactForm.valid) {
+  //     const contact: Contact = this.contactForm.value as Contact;
+  //     this.http.post('http://localhost:8080/api/contact', contact)
+  //       .subscribe({
+  //         next: (response) => {
+  //           console.log('Contact message saved', response);
+  //           this.toastr.success('Votre message a été envoyé avec succès.');
+
+
+  //           this.errorMessage = '';
+  //           this.contactForm.reset();
+
+  //         },
+  //         error: (error) => {
+  //           console.error('Error saving contact message', error);
+  //           this.errorMessage = 'Une erreur est survenue lors de l\'envoi du message. Réessayez !';
+  //           // this.toastr.error('Une erreur est survenue lors de l\'envoi du message. Réessayez');
+
+  //         }
+  //       });
+  //   }
+  // }
 }
