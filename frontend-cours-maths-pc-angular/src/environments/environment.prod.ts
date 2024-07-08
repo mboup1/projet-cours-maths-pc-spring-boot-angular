dbaccd1f0420b67b76e98c1283dd2e4795ce840d
mboup <mboup124@gmail.com>
2024-07-08 15:21:43 +0200

    dotenv installation

--- frontend-cours-maths-pc-angular/src/environments/environment.prod.ts
+++
@@ -1,7 +0,0 @@
-// src/environments/environment.prod.ts
-export const environment = {
-  production: true,
-  emailJsServiceId: 'votre_service_id_prod',
-  emailJsTemplateId: 'votre_template_id_prod',
-  emailJsUserId: 'votre_user_id_prod'
-};
--- frontend-cours-maths-pc-angular/src/environments/environment.ts
+++
@@ -1,7 +0,0 @@
-// src/environments/environment.ts
-export const environment = {
-  production: false,
-  emailJsServiceId: 'service_n2t0sj7',
-  emailJsTemplateId: 'template_29c199i',
-  emailJsUserId: 'Z2rmah8bXQzQjaIRq'
-};
