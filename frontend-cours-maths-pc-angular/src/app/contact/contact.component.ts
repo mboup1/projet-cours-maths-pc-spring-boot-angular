80d9b72f9a754c4bf82d69e9f9ce503179bd6595
mboup <mboup124@gmail.com>
2024-09-30 11:19:36 +0200

    edit application.properties

--- frontend-cours-maths-pc-angular/src/app/contact/contact.component.ts
+++ frontend-cours-maths-pc-angular/src/app/contact/contact.component.ts
@@ -27,52 +27,52 @@ export class ContactComponent {
 
   constructor(private http: HttpClient, private toastr: ToastrService) { }
 
-  onSubmit() {
-    if (this.contactForm.valid) {
-      const templateParams = {
-        from_name: this.contactForm.get('name')?.value,
-        reply_to: this.contactForm.get('email')?.value,
-        phone: this.contactForm.get('phone')?.value,
-        message: this.contactForm.get('message')?.value,
-      };
-
-      emailjs.send(environment.emailJsServiceId, environment.emailJsTemplateId, templateParams, environment.emailJsUserId)
-      // emailjs.send('service_n2t0sj7', 'template_29c199i', templateParams, 'Z2rmah8bXQzQjaIRq')
-        .then((result: EmailJSResponseStatus) => {
-          console.log(result.text);
-          this.toastr.success('Votre message a été envoyé avec succès.');
-          this.errorMessage = '';
-          this.contactForm.reset();
-        }, (error) => {
-          console.error(error.text);
-          this.errorMessage = 'Une erreur est survenue lors de l\'envoi du message. Réessayez !';
-          this.toastr.error('Une erreur est survenue lors de l\'envoi du message. Réessayez');
-        });
-    }
-  }
-
-
   // onSubmit() {
   //   if (this.contactForm.valid) {
-  //     const contact: Contact = this.contactForm.value as Contact;
-  //     this.http.post('http://localhost:8080/api/contact', contact)
-  //       .subscribe({
-  //         next: (response) => {
-  //           console.log('Contact message saved', response);
-  //           this.toastr.success('Votre message a été envoyé avec succès.');
+  //     const templateParams = {
+  //       from_name: this.contactForm.get('name')?.value,
+  //       reply_to: this.contactForm.get('email')?.value,
+  //       phone: this.contactForm.get('phone')?.value,
+  //       message: this.contactForm.get('message')?.value,
+  //     };
 
-
-  //           this.errorMessage = '';
-  //           this.contactForm.reset();
-
-  //         },
-  //         error: (error) => {
-  //           console.error('Error saving contact message', error);
-  //           this.errorMessage = 'Une erreur est survenue lors de l\'envoi du message. Réessayez !';
-  //           // this.toastr.error('Une erreur est survenue lors de l\'envoi du message. Réessayez');
-
-  //         }
+  //     emailjs.send(environment.emailJsServiceId, environment.emailJsTemplateId, templateParams, environment.emailJsUserId)
+  //     // emailjs.send('service_n2t0sj7', 'template_29c199i', templateParams, 'Z2rmah8bXQzQjaIRq')
+  //       .then((result: EmailJSResponseStatus) => {
+  //         console.log(result.text);
+  //         this.toastr.success('Votre message a été envoyé avec succès.');
+  //         this.errorMessage = '';
+  //         this.contactForm.reset();
+  //       }, (error) => {
+  //         console.error(error.text);
+  //         this.errorMessage = 'Une erreur est survenue lors de l\'envoi du message. Réessayez !';
+  //         this.toastr.error('Une erreur est survenue lors de l\'envoi du message. Réessayez');
   //       });
   //   }
   // }
+
+
+  onSubmit() {
+    if (this.contactForm.valid) {
+      const contact: Contact = this.contactForm.value as Contact;
+      this.http.post('http://localhost:8080/api/contact', contact)
+        .subscribe({
+          next: (response) => {
+            console.log('Contact message saved', response);
+            this.toastr.success('Votre message a été envoyé avec succès.');
+
+
+            this.errorMessage = '';
+            this.contactForm.reset();
+
+          },
+          error: (error) => {
+            console.error('Error saving contact message', error);
+            this.errorMessage = 'Une erreur est survenue lors de l\'envoi du message. Réessayez !';
+            // this.toastr.error('Une erreur est survenue lors de l\'envoi du message. Réessayez');
+
+          }
+        });
+    }
+  }
 }
