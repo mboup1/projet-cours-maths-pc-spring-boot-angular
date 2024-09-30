6581eeaa017fa3300723da768f7e66df0c501222
mboup <mboup124@gmail.com>
2024-09-30 13:41:05 +0200

    edit config

--- backend-cours-maths-pc/src/main/java/com/dame/backend_cours_maths_pc/Config/CorsConfig.java
+++ backend-cours-maths-pc/src/main/java/com/dame/backend_cours_maths_pc/Config/CorsConfig.java
@@ -9,9 +9,9 @@ public class CorsConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
+        registry.addMapping("/api/**")
                 .allowedOrigins("http://35.184.173.232:8081")
-//                .allowedOrigins("http://localhost:4200")
+                .allowedOrigins("http://localhost:4200")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
