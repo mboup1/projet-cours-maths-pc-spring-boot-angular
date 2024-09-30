d37cedd7e86699c265b0087e69f0a74a81974a47
mboup <mboup124@gmail.com>
2024-09-30 16:05:01 +0200

    edit config

--- backend-cours-maths-pc/src/main/java/com/dame/backend_cours_maths_pc/controller/ContactMessageController.java
+++ backend-cours-maths-pc/src/main/java/com/dame/backend_cours_maths_pc/controller/ContactMessageController.java
@@ -10,7 +10,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/contact")
 //@CrossOrigin(origins = "http://localhost:4200")
-@CrossOrigin(origins = "http://35.184.173.232:4200")
+//@CrossOrigin(origins = "http://35.184.173.232:4200")
 //@CrossOrigin(origins = "*")
 public class ContactMessageController {
 
